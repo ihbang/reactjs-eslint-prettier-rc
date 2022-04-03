e4d2490e115b222d85b49f0bedcd8e18c4de6c44
ihbang <inhyeok@DESKTOP-862ATTS.localdomain>
2022-04-03 15:00:16 +0900

    modify create-react-app function

--- create-react-app.sh
+++ create-react-app.sh
@@ -1,8 +1,9 @@
 # function definition for create-react-app and eslint config on fish shell
 function create-react-app
     npx create-react-app .
+    npm install --save prop-types
     npx install-peerdeps --dev eslint-config-airbnb
     npm install --save-dev --save-exact prettier
     npm install --save-dev eslint-plugin-prettier eslint-config-prettier
-    cp ~/devel/reactjs-eslint-prettier-rc/*.js .
+    cp ~/devel/reactjs-eslint-prettier-rc/.*.js .
 end
