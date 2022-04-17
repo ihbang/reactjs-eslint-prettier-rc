ab3d516048e680f8c552f66f1e8784ced8a70da5
ihbang <inhyeok@DESKTOP-862ATTS.localdomain>
2022-04-17 19:50:47 +0900

    add create-solana-dapp

--- create-solana-dapp.sh
+++ create-solana-dapp.sh
@@ -1,10 +1,10 @@
 # function definition for create-next-app and eslint config on fish shell
-function create-next-app
+function create-solana-dapp
     if count $argv > /dev/null
-        npx create-next-app@latest $argv --use-npm --ts
+        npx create-next-app@latest $argv --use-npm --ts -e https://github.com/solana-labs/dapp-scaffold
         cd $argv
     else
-        npx create-next-app@latest . --use-npm --ts
+        npx create-next-app@latest . --use-npm --ts -e https://github.com/solana-labs/dapp-scaffold
     end
     npm i -D prettier eslint-config-prettier eslint-plugin-prettier
     npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser
